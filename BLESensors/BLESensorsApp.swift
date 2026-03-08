a3d678d34e50777e2c5e9a7b10dedf5c8b40473d
Marc Santa <mrchrome@gmail.com>
2026-03-08 07:52:59 -0400

    Initial Commit

---
+++ BLESensors/BLESensorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLESensorsApp.swift
+//  BLESensors
+//
+//  Created by Marc Santa on 3/8/26.
+//
+
+import SwiftUI
+
+@main
+struct BLESensorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
